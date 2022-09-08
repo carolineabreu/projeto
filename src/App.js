1976a5ff002f8245ceca1d0d5f86ebb3a1c9d0d6
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2022-09-08 18:13:44 -0300

    form create list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import { CreateList } from "./pages/CreateList";
+import { Home } from "./pages/Home";
+import { ListPage } from "./pages/ListPage";
+
+
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Routes>
+        <Route path="/create-list" element={<CreateList />} />
+      </Routes>
     </div>
   );
 }
