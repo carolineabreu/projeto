938b2ae6bbfe07f09ee906537dec98e6302cd230
Zimarlen Silva <zimarlensilva@gmail.com>
2022-09-08 16:55:18 -0700

    home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+
+
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
