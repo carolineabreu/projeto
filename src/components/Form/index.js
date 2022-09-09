0576aaaebf031fda4b61910d1d8f1dddc291b725
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2022-09-09 15:11:22 -0300

    create form component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,33 +1,69 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 
-export function Home() {
-  const [items, setItems] = useState([]);
+import { useNavigate } from "react-router-dom";
 
-  useEffect(() => {
-    async function fetchItems() {
-      try {
-        const response = await axios.get(
-          "https://ironrest.herokuapp.com/shoppinglist"
-        );
-        setItems([...response.data]);
-      } catch (err) {
-        console.log(err);
-      }
+export function CreateList() {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    productName: "",
+    productQuantity: 0,
+    productImage: ""
+  });
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "https://ironrest.herokuapp.com/shoppinglist",
+        form
+      );
+
+      navigate("/");
+
+      console.log(response);
+    } catch (err) {
+      console.log(err);
     }
-    fetchItems();
-  }, []);
+  }
 
   return (
-    <>
-      {items.map((currentItems) => {
-        return (
-          <Link to={`/${currentItems._id}`}>
-            <h2>Shopping List </h2>{" "}
-          </Link>
-        );
-      })}
-    </>
+    <form onSubmit={handleSubmit}>
+
+      <label htmlFor="product">Product: </label>
+      <input
+        id="product"
+        name="productName"
+        type="text"
+        value={form.productName}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="quantity">Quantity: </label>
+      <input
+        id="quantity"
+        name="productQuantity"
+        type="number"
+        value={form.productQuantity}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="image">Image: </label>
+      <input
+        id="image"
+        name="productImage"
+        type="text"
+        value={form.productImage}
+        onChange={handleChange}
+      />
+
+      <button type="submit">Send</button>
+    </form>
   );
 }
