63c3a97d782fa27856a048659a23b2061b6c83fc
larissambn <larissameo1@gmail.com>
2022-09-08 22:56:35 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/Navbar/NavBar";
 import { CreateList } from "./pages/CreateList";
 import { Home } from "./pages/Home";
 import { ListPage } from "./pages/ListPage";
 
-
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
         <Route path="/create-list" element={<CreateList />} />
+        <Route path="/ListPage" element={<ListPage />} />
       </Routes>
     </div>
   );
